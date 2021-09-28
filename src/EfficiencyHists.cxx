e9e899e21e7c8d4c1a29d6bfbc6c451e581b44ae
Anna Monika Albrecht <albrecha@naf-cms17.desy.de>
2021-09-28 14:37:25 +0200

    plotting script for efficiency

---
+++ src/EfficiencyHists.cxx
@@ -0,0 +1,63 @@
+#include "UHH2/HOTVRStudies/include/EfficiencyHists.h"
+
+#include "TH1F.h"
+#include "TH2F.h"
+
+using namespace std;
+using namespace uhh2;
+
+
+EfficiencyHists::EfficiencyHists(Context & ctx, const string & dirname, bool is_qcd, TopJetId id , Event::Handle<std::vector<pair<TopJet, TopJet> > > &topjetcollection):
+Hists(ctx, dirname),
+b_is_qcd(is_qcd),
+m_id(id),
+h_matched_pairs(topjetcollection)
+{
+  double pt_intervall[] = {0, 200, 400, 500, 600, 700, 800, 1000, 1200, 1400, 1600, 1800, 2000};
+
+  pt_top = book<TH1F>("pt_gentop", "p_{T, topjet}", 12, pt_intervall);
+  pt_matched = book<TH1F>("pt_top_matched", "p_{T, topjet}", 12, pt_intervall);
+  pt_mismatched = book<TH1F>("pt_top_mismatched", "p_{T, topjet}", 12, pt_intervall);
+  pt_gen = book<TH1F>("pt_gen", "p_{T, gen}", 12, pt_intervall);
+
+}
+
+void EfficiencyHists::fill(const Event & event){
+
+  double weight = event.weight;
+  //specific handle to get the matched jets
+  const vector<pair<TopJet, TopJet>> topjets = event.get(h_matched_pairs);
+
+  if (b_is_qcd)
+  {
+    //mismatched jets
+    for(auto matched : topjets)
+    {
+      TopJet topjet = matched.first;
+      TopJet gen = matched.second;
+      pt_gen->Fill(gen.pt(), weight); // fill one hist before matching
+      double pt = -1;
+      if(m_id(topjet, event)) pt = gen.pt(); // take the pt of the corresponding parton jet if a match is found
+      if (pt < 0) continue; // no match found
+      if (pt < 2000) pt_mismatched->Fill(pt, weight);
+      else           pt_mismatched->Fill(1999., weight);
+    }
+  }
+  else
+  {
+    //matched jets
+    for(auto matched : topjets)
+    {
+      TopJet topjet = matched.first;
+      TopJet gen = matched.second;
+      pt_top->Fill(gen.pt(), weight); // fill one hist before matching
+      double pt = -1;
+      if(m_id(topjet, event)) pt = gen.pt(); // take the pt of the corresponding parton jet if a match is found
+      if (pt < 0) continue; // no match found
+      if (pt < 2000) pt_matched->Fill(pt, weight);
+      else           pt_matched->Fill(1999., weight);
+    }
+  }
+}
+
+EfficiencyHists::~EfficiencyHists() {}
